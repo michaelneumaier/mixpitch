f179a91626715be7d32acbca342b310c0112953f
michaelneumaier <mikeneumaier@gmail.com>
2023-10-07 01:21:57 -0400

    fix overflow on mobile. fix/align header nav

--- resources/views/livewire/projects-component.blade.php
+++ resources/views/livewire/projects-component.blade.php
@@ -2,12 +2,12 @@
     <link href="{{ asset('css/projects-page.css') }}" rel="stylesheet">
     <div class="text-4xl text-center text-white mb-6">Projects</div>
 
-    <div class="flex flex-wrap -mx-4">
+    <div class="flex flex-wrap mx-1">
         <!-- Filters Column -->
         <div class="w-full md:w-1/4 lg:w-1/6 px-2">
             <div class="block md:hidden" x-data="{ open: false }">
                 <button @click="open = !open"
-                        class="w-full mb-3 text-white bg-transparent border border-gray-300 rounded-none hover:bg-gray-100">
+                    class="w-full mb-3 text-white bg-transparent border border-gray-300 rounded-none hover:bg-gray-100">
                     Filters
                 </button>
                 <div x-show="open" class="block md:hidden">
@@ -23,7 +23,7 @@
         <div class="w-full px-4 flex-1">
             <div class="flex flex-wrap">
                 @foreach($projects as $project)
-                    @livewire('project-card', ['project' => $project], key($project->id))
+                @livewire('project-card', ['project' => $project], key($project->id))
                 @endforeach
             </div>
             <div class="flex justify-center mt-4">
@@ -32,4 +32,3 @@
         </div>
     </div>
 </div>
-
