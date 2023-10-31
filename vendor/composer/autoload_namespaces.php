0fe0d40de96576d3b8d679f7161dcd53c74f998a
michaelneumaier <mikeneumaier@gmail.com>
2023-10-31 11:06:12 -0400

    sebdesign state machine install

--- vendor/composer/autoload_namespaces.php
+++ vendor/composer/autoload_namespaces.php
@@ -6,5 +6,6 @@ $vendorDir = dirname(__DIR__);
 $baseDir = dirname($vendorDir);
 
 return array(
+    'SM' => array($vendorDir . '/winzou/state-machine/src'),
     'Detection' => array($vendorDir . '/mobiledetect/mobiledetectlib/namespaced'),
 );
