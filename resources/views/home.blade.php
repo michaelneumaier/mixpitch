ec46b665a00d7e48b20ecbb9273b0bd7c323c8c3
michaelneumaier <mikeneumaier@gmail.com>
2023-11-02 20:41:58 -0400

    fix edit project image loading

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="pt-4">
     <div class="py-2 md:px-20">
-        <h1 class="text-3xl md:text-7xl text-secondary text-center grid grid-cols-2 gap-x-4">
+        <h1 class="text-3xl md:text-7xl text-secondary text-center grid grid-cols-2 gap-x-2">
             <div class="text-right">
                 <b>Empower</b><br>
                 <b>Elevate</b><br>
