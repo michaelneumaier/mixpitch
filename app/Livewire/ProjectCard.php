c52c15cb06f76af32384a976a6b6b5e393d84460
michaelneumaier <mikeneumaier@gmail.com>
2023-12-21 19:47:30 -0500

    Added Manage Project

--- app/Livewire/ProjectCard.php
+++ app/Livewire/ProjectCard.php
@@ -14,16 +14,6 @@ class ProjectCard extends Component
         return redirect()->route('projects.show', $this->project);
     }
 
-    public function publish()
-    {
-        $this->project->setStatus('open');
-    }
-
-    public function unpublish()
-    {
-        $this->project->setStatus('unpublished');
-    }
-
     public function render()
     {
         return view('livewire.project-card');
