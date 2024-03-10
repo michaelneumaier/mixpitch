384a6960eddc86640aaf2160f35a3eafb0f7eb0a
michaelneumaier <mikeneumaier@gmail.com>
2024-03-10 18:23:09 -0400

    changed edit/delete project ui

--- resources/views/livewire/project/page/manage-project.blade.php
+++ resources/views/livewire/project/page/manage-project.blade.php
@@ -28,72 +28,6 @@
                         <div class="md:hidden absolute top-0 right-0">
                             <x-project-status-button :status="$project->status" type="top-right" />
                         </div>
-                        <!-- Edit/Delete if User's Project-->
-                        @if(auth()->check() && $project->isOwnedByUser(auth()->user()))
-                        <div class="absolute bottom-0 right-2 flex items-start py-2">
-                            <form action="{{ route('projects.edit', $project) }}" method="GET" class="mr-2">
-                                @csrf
-                                <button type="submit" onclick="event.stopPropagation(); /* handle button click */"
-                                    class="btn-sm btn-warning btn-icon flex text-black items-center space-x-1 px-2 py-2 shadow-md transition-shadow hover:shadow-lg rounded-lg">
-                                    <i class="fas fa-edit"></i>
-                                    <span>Edit</span>
-                                </button>
-                            </form>
-                            <div x-data="{ open: false }">
-                                <!-- Trigger -->
-                                <button @click="open = true"
-                                    onclick="event.stopPropagation(); /* handle button click */"
-                                    class="btn-sm btn-error btn-icon text-white font-bold flex items-center space-x-1 px-2 py-2 shadow-md transition-shadow hover:shadow-lg rounded-lg">
-                                    <i class="fas fa-trash"></i>
-                                    <span>Delete</span>
-                                </button>
-
-                                <!-- Modal -->
-                                <div x-show="open" x-cloak class="fixed z-10 inset-0 overflow-y-auto"
-                                    aria-labelledby="modal-title" role="dialog" aria-modal="true"
-                                    @click="$event.stopPropagation()">
-                                    <div class="flex items-center justify-center min-h-screen">
-                                        <!-- Background overlay -->
-                                        <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
-                                            aria-hidden="true"></div>
-
-                                        <!-- Modal -->
-                                        <span class="hidden sm:inline-block sm:align-middle sm:h-screen"
-                                            aria-hidden="true">&#8203;</span>
-                                        <div
-                                            class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
-                                            <div
-                                                class="bg-white rounded-lg text-left overflow-hidden shadow-xl p-4 transform transition-all sm:align-middle sm:max-w-lg sm:w-full">
-                                                <h3 class="text-lg leading-6 font-medium text-gray-900"
-                                                    id="modal-title">
-                                                    Confirm Deletion
-                                                </h3>
-                                                <div class="mt-2">
-                                                    <p class="text-sm text-gray-500">
-                                                        Are you sure you want to delete this project?
-                                                    </p>
-                                                </div>
-                                            </div>
-                                            <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-                                                <!-- Confirm Button -->
-                                                <form action="{{ route('projects.destroy', $project) }}" method="POST">
-                                                    @csrf
-                                                    @method('DELETE')
-                                                    <button type="submit" class="btn-sm bg-red-400 border">
-                                                        Confirm
-                                                    </button>
-                                                </form>
-                                                <!-- Cancel Button -->
-                                                <button @click="open = false" class="btn-sm border">
-                                                    Cancel
-                                                </button>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        @endif
 
                         <!-- The actual lightbox overlay -->
                         @if($project->image_path)
@@ -163,6 +97,13 @@
                                     <x-project-status-button :status="$project->status" type="top-right" />
                                 </div>
                             </div>
+                        </div>
+                        <div class="flex w-full">
+                            <a href="{{ route('projects.edit', $project)}}" class="block bg-warning/80 hover:bg-warning tracking-tight text-xl text-center font-bold
+                                                    grow py-2 px-4 shadow-accent hover:shadow-accent-focus
+                                                    whitespace-nowrap">
+                                <i class="fas fa-edit"></i>
+                                Edit Project Details</a>
                         </div>
                         <div class="font-sans w-full border-t border-b border-base-200">
                             <div class="flex flex-row">
@@ -286,6 +227,59 @@
                             wire:click="unpublish()">
                             Unpublish</div>
                         @endif
+                    </div>
+
+                    <div class="flex flex-col bg-base-200 rounded-lg" x-data="{ open: false }">
+
+                        <span class="flex-row grow text-xl font-bold p-4">Permanently delete project and all
+                            files</span>
+                        <!-- Modal -->
+                        <div x-show="open" x-cloak class="fixed z-10 inset-0 overflow-y-auto"
+                            aria-labelledby="modal-title" role="dialog" aria-modal="true"
+                            @click="$event.stopPropagation()">
+                            <div class="flex items-center justify-center min-h-screen">
+                                <!-- Background overlay -->
+                                <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
+                                    aria-hidden="true"></div>
+
+                                <!-- Modal -->
+                                <span class="hidden sm:inline-block sm:align-middle sm:h-screen"
+                                    aria-hidden="true">&#8203;</span>
+                                <div
+                                    class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+                                    <div
+                                        class="bg-white rounded-lg text-left overflow-hidden shadow-xl p-4 transform transition-all sm:align-middle sm:max-w-lg sm:w-full">
+                                        <h3 class="text-lg leading-6 font-medium text-gray-900" id="modal-title">
+                                            Confirm Deletion
+                                        </h3>
+                                        <div class="mt-2">
+                                            <p class="text-sm text-gray-500">
+                                                Are you sure you want to delete this project?
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+                                        <!-- Confirm Button -->
+                                        <form action="{{ route('projects.destroy', $project) }}" method="POST">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="submit" class="btn-sm bg-red-400 border">
+                                                Confirm
+                                            </button>
+                                        </form>
+                                        <!-- Cancel Button -->
+                                        <button @click="open = false" class="btn-sm border">
+                                            Cancel
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="btn bg-error/80 hover:bg-error flex-row rounded-t-none text-white text-center"
+                            @click="open = true" onclick="event.stopPropagation(); /* handle button click */">
+                            Delete Project</div>
+
                     </div>
                 </div>
             </div>
